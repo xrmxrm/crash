7594062ab0b47d3a82cda5dce96dfdc56885bc28
richard <xrmxrm@gmail.com>
2024-02-22 12:13:16 -0800

    Save changes

--- ch07/parrot.py
+++ ch07/parrot.py
@@ -1,11 +1,8 @@
 prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
+prompt += "\nEnter to end the program. "
 
 active = True
-while active:
-    message = input(prompt)
-
-    if message == 'quit':
-        active = False
-    else:
+while message := input(prompt):
         print(message)
+
+   
