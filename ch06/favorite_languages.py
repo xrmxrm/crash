61f6a35ce15858e3b5b184df058ea5849451c7e5
richard <xrmxrm@gmail.com>
2024-02-22 10:38:22 -0800

    Save changes

--- ch06/favorite_languages.py
+++ ch06/favorite_languages.py
@@ -1,4 +1,5 @@
 favorite_languages = {
+      'horace': [],
       'jen': ['python', 'rust'],
       'sarah': ['c'],
       'edward': ['rust', 'go'],
@@ -6,6 +7,24 @@ favorite_languages = {
       }
 
 for name, languages in favorite_languages.items():
-    print(f"\n{name.title()}'s favorite languages are:")
-    for language in languages:
-        print(f"\t{language.title()}")
+    test_len = len(languages)
+    if test_len == 0:
+        print(f"\n{name.title()}'doesn't play favorites.")
+    elif test_len == 1:
+        print(f"\n{name.title()}'s favorite languages is {languages[0]}")
+    else:
+        print(f"\n{name.title()}'s favorite languages are:")
+        for language in languages:
+            print(f"\t{language.title()}", end=' ')
+        
+    print("\n\n")
+
+people_to_poll = ['jen', 'jack', 'sally', 'phil', 'george']
+
+for name in people_to_poll:
+    if name in favorite_languages.keys():
+        print(f"Thanks, {name.title()}, for responding!")
+    else:
+        print(f"{name.title()}, please take the survey. Thanks.")
+
+print("\n\n")
