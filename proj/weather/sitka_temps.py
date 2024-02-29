22403c34d613f3ac99046f354cc84eb5db9d2052
richard <xrmxrm@gmail.com>
2024-02-28 17:36:08 -0800

    Add low temps

--- proj/weather/sitka_temps.py
+++ proj/weather/sitka_temps.py
@@ -1,29 +1,37 @@
 from pathlib import Path
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
 BASE='/home/richard/Dev/Py/crash/proj/weather'
 
-path = Path(f'{BASE}/weather_data/sitka_weather_07-2021_simple.csv')
+path = Path(f'{BASE}/weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
+
 # Extract high temperatures.
-highs = []
+dates, highs, lows = [], [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
+    dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # Plot the high temperatures.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # Format plot.
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
